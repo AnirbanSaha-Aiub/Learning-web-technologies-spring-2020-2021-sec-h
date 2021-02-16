3fa1d47690ac8e972bc39a646592f994550c24eb
Anirban <anirbansaha002@gmail.com>
2021-02-16 15:02:19 +0600

    Tast_4 Task_C

---
+++ MID_LAB_TASK_PHP_05/TASK_4/TASK_C/gender.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form class="" action="gender.php" method="post">
+      <fieldset>
+
+
+        <legend>Grnder</legend>
+            <input type="radio" name="gender" value="<?php if(isset($_POST['gender'])){ echo $_POST['gender']; }?> "> male
+            <input type="radio" name="gender" value="<?php if(isset($_POST['gender'])){ echo $_POST['gender']; }?> "> Female
+            <input type="radio" name="gender" value="male"> Other
+            <br>
+        <input type="submit" name="submit" value="Submit"><br>
+      </fieldset>
+    </form>
+  </body>
+</html>
