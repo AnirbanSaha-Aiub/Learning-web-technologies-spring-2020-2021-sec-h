9df6a0bc66cc89a669d741637cd8b326fdd84239
Anirban <anirbansaha002@gmail.com>
2021-02-16 14:42:25 +0600

    Tast_3 Task_A

---
+++ MID_LAB_TASK_PHP_05/TASK_3/TASK_A/dob.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST['submit']))
+	{
+		$name = $_POST['dob'];
+
+		if($name == ""){
+			echo "null submission";
+		}else{
+			echo "success...";
+		}
+	}else{
+		echo "Invalid request...";
+	}
+
+?>
