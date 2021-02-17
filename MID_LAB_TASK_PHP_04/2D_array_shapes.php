9e329e4a77ef653824fcccfc06fda248dff2adff
Anirban <anirbansaha002@gmail.com>
2021-02-17 18:32:28 +0600

    2d array final

---
+++ MID_LAB_TASK_PHP_04/2D_array_shapes.php
@@ -0,0 +1,30 @@
+<?php
+    $_array = array(
+        array('1','2','3','A'),
+        array('1','2','B','C'),
+        array('1','D','E','F'),
+    );
+
+    $temp = 2;
+    for($i = 0; $i<3; $i++)
+    {
+        for($j = 0; $j <3-$i; $j++)
+        {
+            echo $_array[$i][$j];
+        }
+        echo "\n";
+    }
+
+    echo "\n";
+    $temp = 2;
+    for($i = 0; $i<3; $i++)
+    {
+        for($j = 3-$i; $j <4; $j++)
+        {
+            echo $_array[$i][$j];
+
+        }
+        echo "\n";
+    }
+
+?>
