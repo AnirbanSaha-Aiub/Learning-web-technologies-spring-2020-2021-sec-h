d982415b243899d8d5753a413aa6eaecef32d602
Anirban <anirbansaha002@gmail.com>
2021-02-23 12:45:52 +0600

    Profile Picture page

---
+++ MID_LAB_TASK_PHP_06/ProfilePicture.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>LOGIN</title>
+  </head>
+  <body>
+    <form class="" action="" method="post">
+      <fieldset>
+        <legend>Profile Picture</legend>
+        <input type="file" name="file" value="">
+        <br>
+        <br>
+        <input type="submit" name="submit" value="Submit">
+      </fieldset>
+    </form>
+  </body>
+</html>
