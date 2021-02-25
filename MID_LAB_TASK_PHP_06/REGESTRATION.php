11edf21abafc1bc5a830d6c3aebc3c462e347941
Anirban <anirbansaha002@gmail.com>
2021-02-25 15:08:55 +0600

    Regestration done

--- MID_LAB_TASK_PHP_06/REGESTRATION.php
+++ MID_LAB_TASK_PHP_06/REGESTRATION.php
@@ -1,8 +1,40 @@
+<?php
+  if(isset($_POST['submit']))
+  {
+
+    		$name = $_POST['name'];
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $gender = $_POST['gender'];
+        $confirm_password =$_POST['confirm_password'];
+        $mm = $_POST['dd'];
+        $dd = $_POST['mm'];
+        $yyyy = $_POST['yyyy'];
+
+    		 if($name == "" && $email == "" && $username == "" && $password == ""  && $dd == "" && $mm == "" && $yyyy == ""){
+    		 	echo "null submission";
+    		 }
+        elseif($yyyy = range(1900,2021) && $dd = range(1,31) && $mm = range(1,12) && $password==$confirm_password){
+    			echo "Successfully submitted";
+
+        }
+        else {
+          echo "Invalid";
+          }
+
+  }
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>REGESTRATION</title>
   </head>
   <body>
     <form class="" action="" method="post">
@@ -26,12 +58,16 @@
               <td>:<input type="password" name="password" value=""></td>
             </tr>
             <tr>
+              <td>Confirm Password</td>
+              <td>:<input type="password" name="confirm_password" value=""></td>
+            </tr>
+            <tr>
               <td colspan="2">
                 <fieldset>
                     <legend>Gender</legend>
-                    <input type="radio" name="gender" value="">Male
-                    <input type="radio" name="gender" value="">Female
-                    <input type="radio" name="gender" value="">Other
+                    <input type="radio" name="gender" value="m">Male
+                    <input type="radio" name="gender" value="f">Female
+                    <input type="radio" name="gender" value="o">Other
                 </fieldset>
               </td>
             </tr>
@@ -39,9 +75,9 @@
               <td colspan="2">
                 <fieldset>
                   <legend>Date of Birth</legend>
-                    <input size="2" type="text" name="dob" value="">/
-                    <input size="2" type="text" name="dob" value="">/
-                    <input size="4" type="text" name="dob" value=""> (dd/mm/yyyy)
+                    <input size="2" type="text" name="dd" value="">/
+                    <input size="2" type="text" name="mm" value="">/
+                    <input size="4" type="text" name="yyyy" value=""> (dd/mm/yyyy)
                 </fieldset>
               </td>
             </tr>
