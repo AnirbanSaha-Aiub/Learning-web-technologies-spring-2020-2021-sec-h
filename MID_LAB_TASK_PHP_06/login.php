0bba65eceec9a0ef4bf9cd235d60a4d8339e6931
Anirban <anirbansaha002@gmail.com>
2021-02-25 12:50:30 +0600

    login Page compelte

--- MID_LAB_TASK_PHP_06/login.php
+++ MID_LAB_TASK_PHP_06/login.php
@@ -1,18 +1,66 @@
 <?php
-
-
-
 	if(isset($_POST['submit']))
 	{
 
-		$name = $_POST['name'];
-    $password =$_POST['pass'];
+			if(isset($_POST['name']) && isset($_POST['password']))
+			{
+					$username = $_POST['name'];
+					$password = $_POST['password'];
+					$capitalLetters = false;
+					$smallLetters = false;
+					$numbers = false;
+					$underScore = false;
+					$period = false;
+					$dash = false;
 
-		if($name == "" and $password == ""){
-			echo "null submission";
-		// }elseif($name == "" and $password == ""){
-		// 	echo "success...";
-		}
+					for($i=0;$i<strlen($username);$i++)
+					{
+							if((ord($username[$i]) >= 97 && ord($username[$i]) <= 122) || (ord($username[$i]) >= 65 && ord($username[$i]) <= 90) || (ord($username[$i]) >= 48 && ord($username[$i]) <= 57) || ($username[$i] == '.') || ($username[$i] == '-') || ($username[$i] == '_'))
+							{
+									$capitalLetters = true;
+									$smallLetters = true;
+									$numbers = true;
+									$period = true;
+									$dash = true;
+									$underScore = true;
+							}
+							else{
+									echo "Username must contain alphanumeric characters, period, dash or underscore only";
+									return;
+							}
+
+					}
+					if(strlen($username)<2)
+						{
+							echo "User name is not valid.";
+						}
+					else{
+								true;
+					}
+
+					if(strlen($password)<8)
+						{
+							echo "Password is not strong.";
+						}
+					else{
+								true;
+					}
+					$Ch=false;
+
+					for($j=0;$j<strlen($password);$j++)
+					{
+							if(($password[$j] == '@') || ($password[$j] == '#') || ($password[$j] == '$') || ($password[$j] == '%'))
+							{
+									$Ch=true;
+									break;
+							}
+					}
+					if($Ch==false){
+							echo "Password is not vaild.";
+							return;
+					}
+			}
+
 	}
 ?>
 
@@ -33,7 +81,7 @@
           </tr>
           <tr>
             <td>Password:</td>
-            <td><input type="password" name="pass" value=""></td>
+            <td><input type="password" name="password" value=""></td>
           </tr>
         </table>
         <input type="checkbox" name="" value=""> Remember Me <br>
