1e1ed82f5a644e6b70271fd1186bcae0530e593b
Anirban <anirbansaha002@gmail.com>
2021-03-23 13:45:46 +0600

    change passwoed

--- FINAL_LAB_TASK_03_PHP/view/change_password.php
+++ FINAL_LAB_TASK_03_PHP/view/change_password.php
@@ -7,21 +7,22 @@
 
 
 <center>
-	<form>
+	<form method="post" action="chaPass.php">
 		<table border="0" cellspacing="0" cellpadding="5">
+
 			<tr>
 				<td>
 					<fieldset>
 						<legend>CHANGE PASSWORD</legend>
 						Current Password<br />
-						<input type="password" /><br />
+						<input type="password" name="password" /><br />
 						New Password<br />
-						<input type="password" /><br />
+						<input type="password" name="npass" /><br />
 						Retype New Password<br />
-						<input type="password"/>
+						<input type="password" name="rnpass"/>
 						<hr />
 						<input type="button" value="Change" />
-						<a href="home.html">Home</a>
+						<!-- <a href="home.html">Home</a> -->
 					</fieldset>
 				</td>
 			</tr>
